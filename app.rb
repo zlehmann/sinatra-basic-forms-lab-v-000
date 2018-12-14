14481a8056153d041ed2873b840943a59493edea
Zachary Lehmann <zlehmann@gmail.com>
2018-12-14 15:06:03 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require_relative 'config/environment'
+require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    puts params
+    @puppy = Puppy.new(params["name"], params["breed"], params["age"])
+    erb :display_puppy
+  end
 
 end
